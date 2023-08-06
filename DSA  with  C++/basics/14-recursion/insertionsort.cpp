a71f9efa1dbd7809a0b246b777e3bb668e030508
Swastick Kumar Dey <swastickkumardey@gmail.com>
2023-08-06 18:53:20 +0530

    updated upto recursion

---
+++ DSA  with  C++/basics/14-recursion/insertionsort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+void sort(int arr[], int n)
+{
+    if (n == 0 || n == 1)
+        return;
+
+    sort(arr, n - 1);
+    int temp = arr[n - 1];
+    int j = n - 2;
+    while (j >= 0 && arr[j] > temp)
+    {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+    arr[j + 1] = temp;
+}
+int main()
+
+{
+
+    int arr[6] = {2, 9, 1, 6, 3, 7};
+    sort(arr, 6);
+    cout << "After Insertion sort: " << endl;
+    for (int i = 0; i < 6; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
